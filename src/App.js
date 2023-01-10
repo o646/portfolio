8249f80e8a7923a5cadb9d6a88ca86789e0d7219
Omar TRABELSI <omartrabelsi21589333@gmail.com>
2023-01-10 18:12:12 +0100

    add pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
+import {BrowserRouter,Route} from 'react-router-dom'
 import Navbar from "./components/navbar";
+import home from './pages/home';
+import projects from './pages/projects';
+import contact from './pages/contact';
+import skills from './pages/skills';
 function App() {
   return (
     <div>
       <Navbar />
-    
     <div className="container">
-      <h1>React app</h1>
-      <button className="btn btn-danger">ok</button>
+      
+      <BrowserRouter>
+      <Route path= '/' component={home}  exact/>
+      <Route path= '/projects' component={projects} />
+      <Route path= '/contact' component={contact} />
+      <Route path= '/skills' component={skills} />
+      </BrowserRouter>
       </div>
  </div>
   );
