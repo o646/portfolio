e482462581176272125ab9a224e79d38060444e4
Omar TRABELSI <omartrabelsi21589333@gmail.com>
2023-01-17 01:57:56 +0100

    index.js modification

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-// D:\projects\portfolio\node_modules\bootstrap\dist\css\bootstrap.min.css
+// C:\Users\Ahmed\Projects\22-11\portfolio\node_modules\bootstrap\dist\css\bootstrap.min.css
 import 'bootstrap/dist/css/bootstrap.min.css'
-//D:\projects\portfolio\node_modules\bootstrap\dist\js\bootstrap.bundle.min.js
+// C:\Users\Ahmed\Projects\22-11\portfolio\node_modules\bootstrap\dist\js\bootstrap.bundle.min.js
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// C:\Users\Ahmed\Projects\22-11\portfolio\node_modules\bootstrap-icons\font\bootstrap-icons.css
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
