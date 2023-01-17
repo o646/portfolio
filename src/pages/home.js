6fb4680bd831d7f6a3aa7a424e7576288723430a
Omar TRABELSI <omartrabelsi21589333@gmail.com>
2023-01-17 01:28:36 +0100

    adding the photos to the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import webImage from '../images/photo-2.png'
 
-function home() {
+function Home() {
   return (
-    <div>home</div>
+    <div className="alert alert-info" role="alert">
+      <img src="/photo-1.jpg" alt="Profile" style={{ borderRadius: '50%' }} />
+      <p>Hello. My name is omar I am a Fullsatack Web Developer</p>
+      <img src={webImage} alt='Web' />
+    </div>
   )
 }
 
-export default home
+export default Home
