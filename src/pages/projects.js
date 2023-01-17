44601b41dd88b77c98c537376969589d5834cd53
Omar TRABELSI <omartrabelsi21589333@gmail.com>
2023-01-17 01:46:32 +0100

    adding skillDetails page and updating the skill page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
-function projects() {
+function Skills() {
+  const [technologies, setTechnologies] = useState([])
+  useEffect(() => {
+    axios.get('https://devmastery-assets.vercel.app/technologies.json')
+      .then(respone => setTechnologies(respone.data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
-    <div>projects</div>
+    <div className='row'>
+      {technologies.map(item => (
+        <div className='col-12 col-sm-6 col-md-4 col-lg-3' style={{ marginBottom: 20 }}>
+          <div class="card" style={{ padding: 15 }}>
+            <img src={item.image} class="card-img-top" alt="Tech" />
+            <div class="card-body">
+              <h5 class="card-title">{item.name}</h5>
+              <Link to={`/skills/${item.id}`} class="btn btn-primary">Learn about</Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default projects
+export default Skills
